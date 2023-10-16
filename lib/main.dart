3ec0bb9ade3b81dc35882cd20a8974772de8b430
Kaio Martins <kaio.martins2@aluno.unip.br>
2023-10-15 23:42:09 -0300

    feat: declaracao de rotas globais do app para chamadas posteriores

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:bagagem_smart/screens/AcessDB.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/App.dart';
+import 'package:bagagem_smart/screens/Login.dart';
+import 'package:bagagem_smart/screens/RecuperacaoConta.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,13 +9,20 @@ void main() async {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Bagagem Smart', themeMode: ThemeMode.system, home: AcessDB());
+        title: 'Bagagem Smart',
+        // themeMode: ThemeMode.system,
+        // home: AcessDB(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Login(),
+        '/recuperacao': (context) => RecuperacaoConta(),
+      },
+    );
   }
 }
