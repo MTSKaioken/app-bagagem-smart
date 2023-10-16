40d5dcc7eb5f38db780c094626aa4ff8d1ffb382
Kaio Martins <kaio.martins2@aluno.unip.br>
2023-10-15 23:22:16 -0300

    WIP: Design dashboard com leituras dos dispositivos, pendente: deixar funcional

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'NavbarLateral.dart';
+
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
 
@@ -8,19 +10,40 @@ class Dashboard extends StatelessWidget {
     return MaterialApp(
         title: 'Bagagem Smart',
         home: Scaffold(
+          backgroundColor: Colors.white,
+          drawer: NavbarLateral(),
           appBar: AppBar(
-            title: Center(
-              child: Text(
-                'Dashboard',
-                textDirection: TextDirection.ltr,
-              ),
+            backgroundColor: Colors.white,
+            shadowColor: Colors.transparent,
+            iconTheme: IconThemeData(
+              color: Colors.black, // Defina a cor desejada aqui
             ),
-            backgroundColor: Colors.black87,
           ),
           body: Center(
-            child: Text(
-              'Olá Mundo, tela Dashboard',
-              textDirection: TextDirection.ltr,
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  margin: EdgeInsets.only(top: 24),
+                ),
+                Icon(Icons.no_luggage_rounded),
+                Text('Dispositivo Conectado'),
+                // div com img
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                ),
+                Center(
+                  child: Row(
+                    children: [
+                      Icon(Icons.location_on),
+                      Text('São Paulo, Brazil'),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ));
