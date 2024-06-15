51a7eb705330972523db1f5c51610a9f893e7bfa
Kaio Martins <kaio.martins2@aluno.unip.br>
2024-06-15 14:58:37 -0300

    ajustando imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:bagagem_smart/screens/Login.dart';
 import 'package:bagagem_smart/screens/Cadastro.dart';
-import 'package:bagagem_smart/screens/RecuperacaoConta.dart';
 import 'package:bagagem_smart/screens/Dashboard.dart';
 import 'package:bagagem_smart/screens/HomePage.dart';
+import 'package:bagagem_smart/screens/Login.dart';
+import 'package:bagagem_smart/screens/RecuperacaoConta.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
